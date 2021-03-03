2a276182275863f338c3b8b09108ce97faff6118
Sean Lim <seanl@presentplate.com>
2021-03-03 15:02:45 +1000

    Add the controller methods for import, table and csv import to the routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users do
+  	collection do 
+  		get :import
+  		get :table, path: 'table(/:search)(/:page)(/:sort_column)(/:sort_style)'
+			post :csv_import
+  	end
+  end
 end
