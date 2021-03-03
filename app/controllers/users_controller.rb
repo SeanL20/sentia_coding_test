8b69a0274ad43b23f46e178f652d8a812a8cfa1d
Sean Lim <seanl@presentplate.com>
2021-03-03 16:22:26 +1000

    Add the functionality to call the user model csv import method in the import csv controller method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,11 @@ class UsersController < ApplicationController
 	end
 
 	def csv_import
-		
+    if params[:file]
+    	User.import_csv(params[:file].path)
+		end
+
+    redirect_to url_for(controller: :users, action: :import)
+    return
 	end
 end
